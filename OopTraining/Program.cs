1454ddb19e64931aaa54f6defcf08ec0e472ee52
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-06-14 22:09:13 +0900

    add: カプセル化を学ぶためにBankAccountクラスを作成

---
+++ OopTraining/Program.cs
@@ -0,0 +1,14 @@
+﻿class BankAccount
+{
+    // 残高は外から直接変えられないように private
+    private int balance = 0;
+
+    // 残高の読み取りは OK
+    public int Balance => balance;
+
+    // 入金はこのメソッドを通じてのみ
+    public void Deposit(int amount)
+    {
+        if (amount > 0) balance += amount;
+    }
+}
