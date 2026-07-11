81ffb455d7b5665445ffd5762ef8e7414c575910
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-07-11 23:39:18 +0900

    add: ファイルの読み書きの練習用のプロジェクトを作成した。
    - ファイルの読み込みはやったよ

---
+++ FileTraining/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        String line;
+        try
+        {
+            StreamReader sr = new StreamReader("D:\\Dev\\csharp-tutorial-v1\\FileTraining\\Sample.txt");
+            line = sr.ReadLine();
+            // nullじゃなかったとき
+            while(line != null)
+            {
+                Console.WriteLine(line);
+                // 2回読んでいるのはどうして？
+                line = sr.ReadLine();
+                sr.Close();
+                // どうしてコンソールの入力を読むの？
+                Console.ReadLine();
+            }
+        }catch(Exception e)
+        {
+            // 改行を入れたらここに入ったよ
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("ここはFinallyブロックです");
+        }
+    }
+}
