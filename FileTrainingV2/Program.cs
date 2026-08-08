e19471f30bb4e4df3ff71f6734f0d3526ce95956
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-08-08 22:29:45 +0900

    add: ファイル保存に成功🎉✨️

--- FileTrainingV2/Program.cs
+++ FileTrainingV2/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// お気に入りのファイルを実行場所と同じ場所に作成する処理
+// 確認のためにパスも表示したい
+
+// TODO: カレントディレクトリでもできるかも？
+string dirPath = AppContext.BaseDirectory;
+Console.WriteLine("dirPath" + dirPath);
+
+string favPath = Path.Combine(dirPath, "favorites.json");
+Console.WriteLine("favPath👉️" + favPath);
+//// TODO: これだとフォルダを作っている事になっているかも？
+//DirectoryInfo directoryInfo = Directory.CreateDirectory(favPath);
+
+// JSONの中にテキストを入れてみる
+
+async Task CreateJsonFileAsync()
+{
+    string message = "トマト";
+    await File.WriteAllTextAsync(favPath, message);
+}
+// これはどうしてawait できるの？
+await CreateJsonFileAsync();
+
+Console.WriteLine("保存に成功しました");
