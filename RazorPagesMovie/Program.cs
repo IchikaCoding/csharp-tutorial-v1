49bf49f24e529eab651bea44bc1bcc2db1d2e065
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-08-16 22:59:02 +0900

    add: RazerPagesMovieのInitialCommit

---
+++ RazorPagesMovie/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.");
+
+builder.Services.AddDbContext<RazorPagesMovieContext>(options => options.UseSqlServer(connectionString));
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// ミドルウェアを有効にする処理たち
+app.UseHttpsRedirection();
+
+app.UseRouting();
+// 今回のアプリではこれ不要らしい
+app.UseAuthorization();
+// HTTPとかCSSとかの静的アセットの配信を最適化？
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
+
+app.Run();
