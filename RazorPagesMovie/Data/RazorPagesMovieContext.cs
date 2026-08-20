507e46ff5106e2eeb757dad3a90b3c6865b0057c
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-08-20 22:49:32 +0900

    wip: RazorPagesのチュートリアル（途中）

--- RazorPagesMovie/Data/RazorPagesMovieContext.cs
+++ RazorPagesMovie/Data/RazorPagesMovieContext.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 
+// ここでOptionを渡して、Program.csで設定したconnectionStringとか接続文字列を渡している
 public class RazorPagesMovieContext(DbContextOptions<RazorPagesMovieContext> options) : DbContext(options)
 {
+    // DBのテーブルに対応するもの
     public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; } = default!;
 }
