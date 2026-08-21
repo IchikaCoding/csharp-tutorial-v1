d3586cb78ffa7ddc92165d8e55b2461962e457eb
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-08-21 23:05:54 +0900

    wip: アウトプット用のHelloというページを作成中

--- RazorPagesMovie/Pages/MoviePages/Create.cshtml.cs
+++ RazorPagesMovie/Pages/MoviePages/Create.cshtml.cs
@@ -14,25 +14,31 @@ public class CreateModel : PageModel
         _context = context;
     }
 
+    // Page()でページに必要な情報を初期化するらしい
     public IActionResult OnGet()
     {
         return Page();
     }
 
+    // 画面側の入力欄が、Movie の各プロパティにBindされます
     [BindProperty]
     public Movie Movie { get; set; } = default!;
 
     // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD.
+    // このメソッドが実行される前に入力された値がMovieモデルにバインドされる
     public async Task<IActionResult> OnPostAsync()
     {
+        // 入力値に問題があった場合は、同じページをエラーメッセージ付きで再表示する
         if (!ModelState.IsValid)
         {
+            // 再表示される
             return Page();
         }
-
+        // Entity Framework Coreに「このMovieを追加予定として管理してください」
         _context.Movie.Add(Movie);
+        // DBにインサートする
         await _context.SaveChangesAsync();
-
+        // 一覧ページにリダイレクトしている。更新結果を返すイメージはちょっと違うらしい。
         return RedirectToPage("./Index");
     }
 }
---
+++ RazorPagesMovie/Pages/Practice/Hello.cshtml.cs
@@ -0,0 +1,15 @@
+using System.Dynamic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesMovie.Pages.Practice
+{
+    public class HelloModel : PageModel
+    {
+        // ブラウザからのHTTPのGETリクエストを処理するメソッド
+        public void OnGet()
+        {
+
+        }
+    }
+
+}
