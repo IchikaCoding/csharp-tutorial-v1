5a39ba02ac89390afd6039bc412feb3720de51b3
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-08-22 22:46:35 +0900

    add: フォームを追加して、最小限のPost処理を実装成功🎉✨️✨️

---
+++ RazorPagesMovie/Pages/Practice/Favorite.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesMovie.Pages.Practice
+{
+    public class FavoriteModel : PageModel
+    {
+        [BindProperty]
+        public string Name { get; set; } = "";
+        public string ResultMessage { get; private set; } = "";
+        public void OnGet() { }
+        public void OnPost()
+        {
+            ResultMessage = $"{Name}さん、Post成功しましたyo🎉";
+            // コンソールに表示されないのはなぜ？
+            Console.WriteLine(ResultMessage + ResultMessage);
+        }
+    }
+}
