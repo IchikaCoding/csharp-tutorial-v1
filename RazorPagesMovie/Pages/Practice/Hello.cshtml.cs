b04c0201aa33d2f0f63241b682081bc68dc9f743
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-08-22 21:45:40 +0900

    add: csのクラスのプロパティをcshtml側で表示することに成功🎉✨️

--- RazorPagesMovie/Pages/Practice/Hello.cshtml.cs
+++ RazorPagesMovie/Pages/Practice/Hello.cshtml.cs
@@ -1,14 +1,18 @@
 using System.Dynamic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Identity.Client;
 
 namespace RazorPagesMovie.Pages.Practice
 {
     public class HelloModel : PageModel
     {
+        public string? Message { get; private set; }
+        public DateTime DisplayAt { get; private set; }
         // ブラウザからのHTTPのGETリクエストを処理するメソッド
         public void OnGet()
         {
-
+            Message = "これはCSで設定しました。設定した方は「いちかどん」です";
+            DisplayAt = DateTime.Now;
         }
     }
 
