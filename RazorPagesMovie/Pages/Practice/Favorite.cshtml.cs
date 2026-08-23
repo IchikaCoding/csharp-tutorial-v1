444a8d60d78c61f6c4bee29baba45261f8c6fb1d
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-08-23 20:25:31 +0900

    add: BindPropertyの動きを確認してみる

--- RazorPagesMovie/Pages/Practice/Favorite.cshtml.cs
+++ RazorPagesMovie/Pages/Practice/Favorite.cshtml.cs
@@ -11,6 +11,8 @@ namespace RazorPagesMovie.Pages.Practice
         public void OnGet() { }
         public void OnPost()
         {
+            // Nameプロパティに入力値が入るのは、OnPostが実行される前。
+            // だからここが実行される頃にはNameが入っています
             ResultMessage = $"{Name}さん、Post成功しましたyo🎉";
             Console.WriteLine("ResultMessage: " + ResultMessage);
         }
