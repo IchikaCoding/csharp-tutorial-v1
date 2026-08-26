b98371824c9051b81ae27cd9673a4f9a5fa39d4a
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-08-26 21:21:16 +0900

    add: FavoriteInputクラスにReqiredなどの属性を追加。

--- RazorPagesMovie/Models/FavoriteInput.cs
+++ RazorPagesMovie/Models/FavoriteInput.cs
@@ -6,14 +6,25 @@ namespace RazorPagesMovie.Models
     {
         // タイトル、category、理由、Rating、ネタバレを含むかどうか（ContainsSpoiler）
         [Display(Name = "あなたの名前")]
+        // 必須なのになかった場合はエラー
+        [Required(ErrorMessage = "名前を入力してください")]
+        // 第一引数は、Maxです
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "名前は2文字以上20文字以内で入力してください。")]
         public string Name { get; set; } = "";
+
         [Display(Name = "推しの料理のタイトル")]
+        [Required(ErrorMessage = "推し料理が入力されてませんが？！")]
+        [StringLength(60, MinimumLength = 1, ErrorMessage = "料理名は1文字以上、60文字以内で入力してください。")]
         public string Title { get; set; } = "";
         [Display(Name = "料理のカテゴリー")]
+        [Required(ErrorMessage = "カテゴリを選択してください")]
         public string Category { get; set; } = "";
         [Display(Name = "おすすめ理由")]
+        [Required(ErrorMessage = "おすすめ理由が入力されていません")]
         public string Reason { get; set; } = "";
         [Display(Name = "おすすめ度")]
+        [Required(ErrorMessage = "おすすめ度が入力されていません")]
+        [Range(0, 10)]
         public int Rating { get; set; }
         [Display(Name = "ネタバレを含む")]
         public bool ContainsSpoiler { get; set; }
