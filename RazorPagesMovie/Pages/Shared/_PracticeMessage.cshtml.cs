4ad4aa530facbfc3ff14a0e7c231c591ea9d4c69
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-08-28 23:47:56 +0900

    add: サーバーサイドの検証をやってみよう

---
+++ RazorPagesMovie/Pages/Shared/_PracticeMessage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesMovie.Pages.Shared
+{
+    public class _PracticeMessageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
