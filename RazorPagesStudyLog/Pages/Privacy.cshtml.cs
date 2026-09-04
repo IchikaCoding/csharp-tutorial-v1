22adc02c10ff3658c868bfafd34e4826c6ec37f8
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-09-04 21:18:17 +0900

    add: RazorPazesStudyLogプロジェクトを新規追加した

---
+++ RazorPagesStudyLog/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesStudyLog.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
