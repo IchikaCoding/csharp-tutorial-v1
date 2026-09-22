c895c945018c01f99e493acd0664cf5d982ad1bd
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-09-22 21:38:57 +0900

    wip: シードデータを追加

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -17,6 +17,37 @@ namespace MvcMovie.Models
                 {
                     return;
                 }
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "名探偵いちかどん！~真実はいつも1つとは限らない~",
+                        // Parseはちょっとよくわからない
+                        ReleaseDate = DateTime.Parse("2020-02-02"),
+                        Genre = "教育系",
+                        Price = 20000,
+                    },
+                    new Movie
+                    {
+                        Title = "キャラメルポップコーンが進む映画",
+                        ReleaseDate = DateTime.Parse("2026-09-22"),
+                        Genre = "食事系",
+                        Price = 1500,
+                    },
+                    new Movie
+                    {
+                        Title = "シードデータってなに？~学ぶもののみぞ知る~",
+                        ReleaseDate = DateTime.Parse("2000-09-22"),
+                        Genre = "IT教育系",
+                        Price = 20000,
+                    },
+                    new Movie
+                    {
+                        Title = "仕事が遅い！！！！",
+                        ReleaseDate = DateTime.Parse("1600-09-22"),
+                        Genre = "歴史系",
+                        Price = 200000,
+                    }
+                    );
             }
         }
     }
