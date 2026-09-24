e30f0b87afb984f0967ae56b08e46828a227ed4d
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-09-24 22:33:37 +0900

    wip: MvcMovieContextにResidentsモデルを追加。ResidentSeedDataのInitializeメソッドの処理が途中

---
+++ MvcMovie/Models/ResidentSeedData.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+namespace MvcMovie.Models;
+
+public class ResidentSeedData
+{
+    // 初期化メソッドを作成する
+    // そのなかで、もしDBのデータが存在していたら、return
+    // データがなかった場合は、Residentクラスのインスタンスを作る
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        // TODO: ここのエラーを解消する！
+        var context = new MvcMovieContext(DbContextOptions < MvcMovieContext > options);
+    }
+}
--- MvcMovie/MvcMovieContext.cs
+++ MvcMovie/MvcMovieContext.cs
@@ -3,4 +3,6 @@ using Microsoft.EntityFrameworkCore;
 public class MvcMovieContext(DbContextOptions<MvcMovieContext> options) : DbContext(options)
 {
     public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    // TODO: このdefault!ってなんだろう？
+    public DbSet<MvcMovie.Models.Resident> Resident { get; set; } = default!;
 }
