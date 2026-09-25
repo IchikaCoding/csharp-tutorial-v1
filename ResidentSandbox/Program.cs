23bfc25db29ec4a08698833d6eb967533f13f41d
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-09-25 23:31:03 +0900

    add: 住民データ確認用コンソールアプリを作成。
    - インスタンスがしっかり登録出来ているのか確認するために作ってみました。

---
+++ ResidentSandbox/Program.cs
@@ -0,0 +1,23 @@
+﻿using ResidentSandbox;
+
+Console.WriteLine("Hello, World!");
+
+
+var Alpaca = new Resident()
+{
+    Name = "アルパカどん🦙",
+    AnimalType = "Animal",
+    Job = "窓から人類観察",
+    ResidentSince = new DateTime(1600, 9, 26)
+};
+
+var SatsumaimoPan = new Resident()
+{
+    Name = "さつまいもパンまん🍠",
+    AnimalType = "Human",
+    Job = "パン屋さん",
+    ResidentSince = DateTime.Now
+};
+
+Console.WriteLine(Alpaca.Name);
+Console.WriteLine(SatsumaimoPan.Name);
---
+++ ResidentSandbox/Resident.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResidentSandbox
+{
+    public class Resident
+    {
+        // 名前、種類、職業、住民になった日付
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string? AnimalType { get; set; }
+        public string? Job { get; set; }
+        public DateTime ResidentSince { get; set; }
+    }
+}
